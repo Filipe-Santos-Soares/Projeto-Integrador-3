fc3a1d0eece2a2a4c33288de06bd2e431021fe70
RayFarias13 <1313ray@gmail.com>
2025-12-01 16:50:28 -0300

    atualizando models

--- projetointegrador3/concessionaria/models.py
+++ projetointegrador3/concessionaria/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PerfilCliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    #user_id = models.ForeignKey(8888, on_delete = models.CASCADE)
+    cnh = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=13)
+    endereco = models.TextField()
+
+    def __str__(self):
+        return self.id_cliente
+
+class Carro(models.Model):
+    carro_status = [
+        ('Livre','Livre'),
+        ('Alugado','Alugado'),
+        ('Manutenção','Manutenção')
+    ]
+
+
+    id_carro = models.AutoField(primary_key=True)
+    modelo = models.CharField(max_length= 50)
+    placa = models.CharField(7)
+    ano = models.CharField(4)
+    status = models.CharField(choices=carro_status)
+
+    def __str__(self):
+        return self.id_carro
+
+class Aluguel(models.Model):
+    id_aluguel = models.AutoField(primary_key=True)
+    perfil_cliente_id = models.ForeignKey(PerfilCliente, on_delete= models.CASCADE)
+    carro_id = models.ForeignKey(Carro, on_delete=models.CASCADE)
+    #funcionario_id = ()
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    valor = models.IntegerField()
+
+    def __str__(self):
+        return self.id_aluguel
+
+
+

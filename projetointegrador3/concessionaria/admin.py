a6365ec326c466b3ccf209cb085d698607a9d02f
RayFarias13 <1313@gmail.com>
2025-12-04 13:53:02 -0300

    atualizacao 0412 1353

--- projetointegrador3/concessionaria/admin.py
+++ projetointegrador3/concessionaria/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+from rest_framework.authtoken.models import Token
 
-# Register your models here.
+@admin.register(PerfilCliente)
+class PerfilAdmin (admin.ModelAdmin):
+    list_display = ["user"]
+    search_fields = ['user']
+
+
+@admin.register(Carro)
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ["modelo","placa","ano","status"]
+    search_fields = ["modelo","placa","ano","status"]
+    list_filter = ["modelo","status"]
+
+
+admin.site.register(Token)

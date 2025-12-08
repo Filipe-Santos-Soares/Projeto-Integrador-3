bd0b5dd15912d6709e23bf9f5d90223b015e8ec0
RayFarias13 <1313ray@gmail.com>
2025-12-08 15:05:43 -0300

    atualizacao1505 081225

--- projetointegrador3/projetointegrador3/urls.py
+++ projetointegrador3/projetointegrador3/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from concessionaria.views import *
 from rest_framework.authtoken.views import obtain_auth_token
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView 
 router = DefaultRouter()
 router.register(r'user', UserViewSet)
 router.register(r'perfis-cliente', PerfilClienteViewSet)
@@ -30,4 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/',obtain_auth_token),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), 
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), 
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'), 
+
 ]
